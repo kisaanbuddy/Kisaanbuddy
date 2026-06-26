0f4b2da005e4dc764405953216540de0d08aa82d
utkarshsinha-dev <utkarsh.sinha.dev@gmail.com>
2026-06-26 21:36:13 +0530

    fix: allow backend in CSP

--- frontend-next/next.config.mjs
+++ frontend-next/next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://adservice.google.com https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://images.unsplash.com https://plus.unsplash.com https://pagead2.googlesyndication.com; connect-src 'self' https://pagead2.googlesyndication.com; frame-src 'self' https://googleads.g.doubleclick.net https://pagead2.googlesyndication.com;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://adservice.google.com https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://images.unsplash.com https://plus.unsplash.com https://pagead2.googlesyndication.com;   connect-src 'self' https://pagead2.googlesyndication.com; https://krishiai-api.onrender.com; frame-src 'self' https://googleads.g.doubleclick.net https://pagead2.googlesyndication.com;"
           }
         ]
       }
